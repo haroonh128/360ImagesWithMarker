442cae5472752912e04fc4e2b2c62c074ddbe322
haroonh128 <haroonh128@yahoo.com>
2023-04-13 21:01:57 +0500

    map viewer apis

--- src/components/mapviewer/mapviewer.component.ts
+++ src/components/mapviewer/mapviewer.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { UUID } from 'angular2-uuid';
+import { MapViewerService } from 'src/services/map-viewer.service';
 
 @Component({
   selector: 'app-mapviewer',
   templateUrl: './mapviewer.component.html',
-  styleUrls: ['./mapviewer.component.css']
+  styleUrls: ['./mapviewer.component.css'],
 })
 export class MapviewerComponent {
+  form = new FormGroup({
+    Id: new FormControl(UUID.UUID()),
+    Name: new FormControl(''),
+    Description: new FormControl(''),
+    ImageId: new FormControl(''),
+    xAxis: new FormControl(''),
+    yAxis: new FormControl(''),
+    IsDeleted: new FormControl(false),
+    CreatedBy: new FormControl(''),
+    ModifiedBy: new FormControl(''),
+    url: new FormControl(''),
+  });
+
   zoom: number = 8;
   map: any;
+  viewer: any = null;
+  showModal: boolean = false;
   // initial center position for the map
   lat: number = 51.673858;
   lng: number = 7.815982;
@@ -15,36 +33,39 @@ export class MapviewerComponent {
     {
       lat: 51.673858,
       lng: 7.815982,
-      label: "A",
+      label: 'A',
       draggable: true,
-      content: "InfoWindow content",
-      color: "#FFFFFF",
-      iconUrl: "http://maps.google.com/mapfiles/ms/micons/gas.png"
+      content: 'InfoWindow content',
+      color: '#FFFFFF',
+      iconUrl: 'http://maps.google.com/mapfiles/ms/micons/gas.png',
     },
     {
       lat: 51.373858,
       lng: 7.215982,
-      label: "B",
+      label: 'B',
       draggable: false,
-      content: "InfoWindow content",
-      color: "blue",
-      iconUrl: "http://maps.google.com/mapfiles/ms/micons/dollar.png"
+      content: 'InfoWindow content',
+      color: 'blue',
+      iconUrl: 'http://maps.google.com/mapfiles/ms/micons/dollar.png',
     },
     {
       lat: 51.723858,
       lng: 7.495982,
-      label: "C",
+      label: 'C',
       draggable: true,
-      content: "InfoWindow content",
-      color: "red",
-      iconUrl: "http://maps.google.com/mapfiles/ms/micons/police.png"
-    }
+      content: 'InfoWindow content',
+      color: 'red',
+      iconUrl: 'http://maps.google.com/mapfiles/ms/micons/police.png',
+    },
   ];
   clickedMarker(label: string, index: number) {
     console.log(`clicked the marker: ${label || index}`);
   }
-  mapClicked(event: any){
-    console.log("map clicked");
+
+  constructor(private mapSer: MapViewerService) {}
+
+  mapClicked(event: any) {
+    console.log('map clicked');
   }
   onMapReady(map: any) {
     var ownRef = this;
@@ -79,8 +100,54 @@ export class MapviewerComponent {
     });
     this.map = map;
   }
-}
 
+  addMapImage = () => {
+    this.form.controls.CreatedBy.setValue(Date.now().toString());
+
+    this.mapSer.addMapPointer(this.form.getRawValue()).then(
+      (res) => {
+        console.log(res);
+        //Adding hotspot/marker in the current image
+        this.viewer.addHotSpot({
+          pitch: this.form.controls.xAxis.value,
+          yaw: this.form.controls.yAxis.value,
+          text: this.form.controls.Name.value,
+        });
+        this.modalToggle();
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+  };
+
+  delMapImage = () => {
+    this.mapSer.deleteMapPointer(this.form.controls.Id.value?.toString()).then(
+      (res) => {
+        console.log(res);
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+  };
+
+  getMapImage = () => {
+    this.mapSer.getMap().subscribe({
+      next(value) {
+        console.log(value);
+      },
+      error(err) {
+        console.log(err);
+      },
+      complete() {},
+    });
+  };
+
+  modalToggle = () => {
+    this.showModal = !this.showModal;
+  };
+}
 
 // just an interface for type safety.
 interface marker {
---
+++ src/services/map-viewer.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MapViewerService {
+  constructor(private afs: AngularFirestore) {}
+
+  addMapPointer = (data: any) => {
+    return this.afs.collection('/MapViewer').add(data);
+  };
+
+  deleteMapPointer = (id?: string) => {
+    return this.afs.doc(`/MapViewer/${id}`).delete();
+  };
+
+  getMapPointers = () => {
+    return this.afs.collection('/MapViewer').snapshotChanges();
+  };
+
+  addMap = (data: any) => {
+    return this.afs.collection('/MapViewer').add(data);
+  };
+
+  deleteMap = (id?: string) => {
+    return this.afs.doc(`/MapViewer/${id}`).delete();
+  };
+
+  getMap = () => {
+    return this.afs.collection('/MapViewer').snapshotChanges();
+  };
+}
